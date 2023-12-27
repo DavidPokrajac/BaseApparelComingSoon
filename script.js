e4998c57f97921220918628a48fc86d7abcbbc10
David <d0pokrajac@gmail.com>
2023-12-27 11:34:23 +0100

    Added logic and styles for wrong email

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 window.addEventListener('load', () => {
-    console.log('Test statement')
+    const emailField = document.getElementById('email-address')
+
+    const emailPattern = new RegExp(
+        /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+        'i'
+    )
+
+    emailField.addEventListener('keyup', (event) => {
+        if (
+            !emailPattern.test(event.target.value) &&
+            event.target.value !== ''
+        ) {
+            event.target.classList.add('wrong-email')
+            event.target.nextElementSibling.classList.add('error-icon')
+            event.target.parentElement.classList.add('error-message')
+        } else {
+            event.target.classList.remove('wrong-email')
+            event.target.nextElementSibling.classList.remove('error-icon')
+        }
+    })
 })
